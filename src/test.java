a8d321355394f499c5b3a2ebe4ee09069a7cfe24
wang-chaochen <321913135@qq.com>
2020-08-04 08:41:50 +0800

    新建

--- src/test.java
+++ src/test.java
@@ -1,2 +1,26 @@
-public class test {
+import java.util.HashMap;
+import java.util.Map;
+
+public class test{
+    public static void main(String[] args) {
+        Solution solution = new test().new Solution();
+        // TO TEST
+        int [] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        System.out.println(solution.twoSum(nums,target));
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            Map<Integer,Integer> mp = new HashMap<>();
+            for(int i = 0; i < nums.length; i++){
+                if(mp.containsKey(target - nums[i])){
+                    return new int[]{i,mp.get(target-nums[i])};
+                }
+                mp.put(nums[i],i);
+            }
+            throw new IllegalArgumentException("No Two Sum Solution!");
+        }
+    }
 }
+
